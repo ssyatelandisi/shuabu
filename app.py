1a191ce4c8965d2804041001f534073568bd013d
ssyatelandisi <ssyatelandisi@163.com>
2024-01-06 13:09:21 +0800

    update

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from hashlib import md5
 from datetime import datetime as dt
+from timeit import timeit
 from urllib import parse, request
-import os, base64, random, json, random
+import os, base64, random, json
 
 # key = md5(encode(`${params.phone}1${params.password}2${params.step}xjdsb${params.time}`))
 
+GETURL = "https://apis.jxcxin.cn/api/mi?user={}&password={}&step={}"
+
 
 class Params:
-    def __init__(self, phone="", password="", step="", **keyargs):
+    def __init__(self, phone="", password="", step="", p_time=None, **keyargs):
         self.phone = phone
         self.password = password
         self.step = step
-        self.time = str(int(dt.timestamp(dt.now())))
+        self.time = p_time if p_time else str(int(dt.timestamp(dt.now())))
         self.key = md5(
             base64.b64encode(
                 f"{self.phone}1{self.password}2{self.step}xjdsb{self.time}".encode()
@@ -19,34 +22,31 @@ class Params:
         ).hexdigest()
 
 
-def fetch(phone, password, step):
-    params = Params(phone=phone, password=password, step=step)
+def fetch(user: str, password: str, step: str):
     ip = f"119.120.{random.randint(1,255)}.{random.randint(1,255)}"
     req = request.Request(
-        "https://admin.shuabu.net/xmphp",
-        data=parse.urlencode(params.__dict__).encode("utf-8"),
+        url=GETURL.format(user, password, step),
+        # data=parse.urlencode(params.__dict__).encode("utf-8"),
         headers={
-            "authority": "api.shuabu.net",
             "accept": "application/json, text/javascript, */*; q=0.01",
             "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6,ko;q=0.5,zh-TW;q=0.4,und;q=0.3,ru;q=0.2",
             "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "origin": "https://yd.shuabu.net",
-            "referer": "https://yd.shuabu.net/",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+            "referer": "https://step.datu520.cn/",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
             "x-forwarded-for": ip,
             "X-Remote-IP": ip,
             "X-Real-IP": ip,
             "X-Originating-IP": ip,
         },
-        method="POST",
+        method="GET",
     )
     try:
         res = request.urlopen(req, timeout=10.0).read()
     except:
-        print(f"请求超时 {phone}")
+        print(f"请求超时 {user}")
         return False
     else:
-        if json.loads(res.decode("utf-8")).get("code") == 200:
+        if str(json.loads(res.decode("utf-8")).get("code")) == "200":
             return True
         else:
             print(json.loads(res.decode("utf-8")))
@@ -54,20 +54,32 @@ def fetch(phone, password, step):
 
 
 def shuabu():
+    """
+    [
+        {
+            "phone": "usr1",
+            "password": "psd1"
+        },
+        {
+            "phone": "usr2",
+            "password": "psd2"
+        }
+    ]
+    """
     if not os.environ.get("DATA"):
         print("不存在环境变量DATA")
         return None
     try:
-        json.loads(os.environ.get("DATA"))
+        data = json.loads(os.environ.get("DATA"))
     except Exception as e:
         print(e)
         return None
     step = str(random.randint(18000, 22000))
-    for item in json.loads(os.environ.get("DATA")):
-        times = 0
-        while times <= 5:
+    for item in data:
+        while (times := 0) <= 5 and not item.get("result", False):  # times失败次数
             result = fetch(item["phone"], item["password"], step)
             if result:
+                item["result"] = True
                 print(f'{item["phone"]} {step}  sucess')
                 break
             else:
@@ -76,7 +88,6 @@ def shuabu():
 
 
 if __name__ == "__main__":
-    start = dt.now()
-    shuabu()
-    finish = dt.now()
-    print(f"耗时 {finish - start}\n")
+    print(
+        f'耗时 {timeit(stmt="shuabu()", setup="from __main__ import shuabu", number=1)}\n'
+    )
